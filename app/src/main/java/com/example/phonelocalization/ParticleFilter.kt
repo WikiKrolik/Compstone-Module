f2b612f3420beaec8abc61a2d97c9893b786ad99
Wiktoria <234762@edu.p.lodz.pl>
2023-01-11 15:49:41 +0100

    CalculateMoement doesnt working :)

--- app/src/main/java/com/example/phonelocalization/ParticleFilter.kt
+++ app/src/main/java/com/example/phonelocalization/ParticleFilter.kt
@@ -13,20 +13,53 @@ import android.provider.Telephony.Mms.Part
 import java.util.*
 import kotlin.math.PI
 import kotlin.math.atan2
-import kotlin.random.Random.Default.nextInt
+import kotlin.math.*
 
 object ParticleFilter {
 
-    fun AvailablePositions( pair: Pair<Int, Int>): Boolean{
+
+    // A simple data class to represent a 2D point with x and y coordinates
+    data class Point(val x: Int, val y: Int)
+
+    // A simple data class to represent a particle with a position (x, y) and weight
+    data class Particle(val position: Point, var weight: Double)
+
+    fun calculateMovement(
+        magX: Double, magY: Double, gyroX: Double, gyroY: Double, accelX: Double, accelY: Double,
+        particles: ArrayList<Particle>, dt: Double
+    ): ArrayList<Particle> {
+        // Predict the new positions of the particles using the gyroscope readings
+        val predictedParticles = particles.map { particle ->
+            val newX = (particle.position.x + gyroX * dt).toInt()
+            val newY = (particle.position.y + gyroY * dt).toInt()
+            Particle(Point(newX, newY), particle.weight)
+        }
+
+        // Update the weights of the particles using the magnetometer and accelerometer readings
+        val updatedParticles = predictedParticles.map { particle ->
+            val errorX = particle.position.x - magX
+            val errorY = particle.position.y - magY
+            val newWeight = exp(-errorX * errorX / 2.0 - errorY * errorY / 2.0)
+            Particle(particle.position, newWeight)
+        }
+
+        // Normalize the weights of the particles
+        val totalWeight = updatedParticles.map { it.weight }.sum()
+        val normalizedParticles = updatedParticles.map { Particle(it.position, it.weight / totalWeight) }
+
+        return normalizedParticles as ArrayList<Particle>
+    }
+
+   public fun AvailablePositions( pair: Point): Boolean{
        // var arr = arrayOf<Pair<Int, Int>>() // frvlstr blank arr
        // for (x in 2..530){
             //for(y in 4..448){
-                if(( pair.first in 2..96 && pair.second in 4 .. 173) ||
-                    (pair.first in 94 .. 398 && pair.second in 124 .. 173) ||
-                    (pair.first in 398 .. 530 && pair.second in 4 .. 42) ||
-                    (pair.first in 453 .. 530 && pair.second in 42 .. 173) ||
-                    (pair.first in 66 .. 450 && pair.second in 390 .. 448) ||
-                    (pair.first in 2 .. 60)) {
+                if(( pair.x in 2..96 && pair.y in 4 .. 173) ||
+                    (pair.x in 94 .. 398 && pair.y in 124 .. 173) ||
+                    (pair.x in 398 .. 530 && pair.y in 4 .. 42) ||
+                    (pair.x in 453 .. 530 && pair.y in 42 .. 173) ||
+                    (pair.x in 66 .. 450 && pair.y in 390 .. 448) ||
+                    (pair.x in 2 .. 60)) {
                     return true
                 }
             //}
@@ -34,11 +67,9 @@ object ParticleFilter {
         return false
     }
     fun convertToCompassValues(): Float {
-        // Determine the magnetometer's hard and soft iron offsets (omitted for simplicity)
-        // Apply the hard and soft iron offsets to the magnetometer readings (omitted for simplicity)
 
         // Calculate the heading using the magnetometer readings and trigonometry
-        var heading = (atan2(SensorReader.Magnetometer.y.toDouble(), SensorReader.Magnetometer.x.toDouble()) *180/ PI + 270).toFloat()
+        var heading = (atan2(SensorReader.Magnetometer.y.toDouble(), SensorReader.Magnetometer.x.toDouble()) *180/ PI - 90).toFloat()
         if (heading < 0 ) {
             heading += 360;
         }
@@ -47,21 +78,22 @@ object ParticleFilter {
         return heading
     }
 
-   fun GeneratePositions(): String {
-       var arr: String = ""
+
+   public fun GeneratePositions(): List<Particle> {
+       //var arr : ArrayList<Particle> = ArrayList<Particle>(3)
+       val list = arrayListOf<Particle>()
+
        var a: Int = 0
-       var x: Int
-       var y : Int
-       while (a <= 1000){
+       while (a <= 2){
            var x: Int =  (0 .. 530).random()
            var y: Int = (0.. 488).random()
-           var pair = Pair(x, y)
-           if (AvailablePositions(pair)){
+           var pair = Particle(Point(x, y), 1.0)
+           if (AvailablePositions(Point(x, y))){
+               list.add(pair)
                a++
-               arr += "$pair;     "
            }
         }
-       return arr;
+       return list;
     }
 
     fun Resample(particles : ArrayList<Particle>){
@@ -85,10 +117,10 @@ object ParticleFilter {
             }
         }
 
-        //Copy the particle with the biggest weight, as many times, as many particles have been deleted in this iteration.
-        for(i in 0..numOfParticlesToRemove){
-            particles.add(Particle(maxParticle.x, maxParticle.y, maxParticle.weight))
-        }
+//        //Copy the particle with the biggest weight, as many times, as many particles have been deleted in this iteration.
+//        for(i in 0..numOfParticlesToRemove){
+//            particles.add(Particle(maxParticle.x, maxParticle.y, maxParticle.weight))
+//        }
 
     }
 }
--- app/src/main/java/com/example/phonelocalization/SecondFragment.kt
+++ app/src/main/java/com/example/phonelocalization/SecondFragment.kt
@@ -2,6 +2,7 @@ package com.example.phonelocalization
 
 import android.hardware.Sensor
 import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.os.Handler
@@ -20,7 +21,8 @@ class SecondFragment : Fragment() {
 
     private var SensorManage: SensorManager? = null
     private var DegreeStart = 0f
-
+    var arr = arrayListOf<ParticleFilter.Particle>()
+    var list = arrayListOf<ParticleFilter.Particle>()
 private var _binding: FragmentSecondBinding? = null
     var handler: Handler = Handler()
     var runnable: Runnable? = null
@@ -35,28 +37,65 @@ private var _binding: FragmentSecondBinding? = null
     ): View? {
       _binding = FragmentSecondBinding.inflate(inflater, container, false)
        // binding.positionData.text = ParticleFilter.GeneratePositions()
-
+         arr = ParticleFilter.GeneratePositions() as ArrayList<ParticleFilter.Particle>
+        binding.positionData.text = ParticleFilter.GeneratePositions().toString()
 
       return binding.root
 
     }
 
 
-//    fun onSensorChanged(event: SensorEvent) {
-//        // get angle around the z-axis rotated
-//        val degree = Math.round(event.values[0]).toFloat()
-//        binding.positionData.text = ("Heading: " + java.lang.Float.toString(degree) + " degrees")
-//        DegreeStart = -degree
-//    }
+    // Sensor event listener
+    val sensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            // Check the sensor type
+            when (event.sensor.type) {
+                Sensor.TYPE_GYROSCOPE -> {
+                    // Convert the raw gyroscope data into angular velocity (degrees per second)
+                    val angularVelocityX = event.values[0]
+                    val angularVelocityY = event.values[1]
+
+                    // Update the particle filter with the gyroscope data
+//                    particleFilter.predict(angularVelocityX, angularVelocityY, dt)
+                }
+                Sensor.TYPE_ACCELEROMETER -> {
+                    // Convert the raw accelerometer data into acceleration (g)
+                    val accelerationX = event.values[0]
+                    val accelerationY = event.values[1]
+
+                    // Update the particle filter with the accelerometer data
+                    //particleFilter.update(accelerationX, accelerationY)
+                }
+                Sensor.TYPE_MAGNETIC_FIELD -> {
+                    // Convert the raw accelerometer data into acceleration (g)
+                    val magnetometerX = event.values[0]
+                    val magnetometerY = event.values[1]
+
+                    // Update the particle filter with the accelerometer data
+                    //particleFilter.update(accelerationX, accelerationY)
+                }
+            }
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+
+
+
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         handler.postDelayed(Runnable {
-            handler.postDelayed(runnable!!, delay.toLong())
 
-            binding.positionData.text = ParticleFilter.convertToCompassValues().toString()
+            handler.postDelayed(runnable!!, delay.toLong())
+             arr = ParticleFilter.calculateMovement(SensorReader.Magnetometer.x.toDouble(), SensorReader.Magnetometer.y.toDouble(),
+                                                SensorReader.Gyroscope.x.toDouble(),SensorReader.Gyroscope.y.toDouble(),
+                                                SensorReader.Accelerometer.x.toDouble(), SensorReader.Accelerometer.y.toDouble(),
+                                                arr, delay.toDouble())
+
+            binding.positionData.text = arr.toString()
         }.also { runnable = it }, delay.toLong())
 
 
